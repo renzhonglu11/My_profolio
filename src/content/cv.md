932ca48acdf464eb8a3c0b7b9f7cfc75942d0d06
renzhonglu11 <lu631992923q@gmail.com>
2022-12-03 23:26:32 +0100

    add mdx

--- src/content/cv.md
+++ src/content/cv.md
@@ -3,6 +3,8 @@ title: CV
 stack: HTML&CSS
 slug: cv
 date: 2022-11-20
+featuredImg: ../iamges/featured/food-banner.png
+thumb: ../images/thumbs/food.png
 ---
 
 
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,28 +2,44 @@ import React from "react";
 import Layout from "../../components/Layout";
 import { graphql, Link } from "gatsby";
 import { portfolio, projects } from "../../styles/projects.module.css";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 export default function Projects(props) {
-  console.log(props.data);
+
   const projectsData = props.data.projects.nodes;
   const contact = props.data.contact.siteMetadata.contact;
-
   return (
     <Layout>
       <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>test</h3>
         <div className={projects}>
-          {projectsData.map(project => (
-            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
-              <div>
-                <h3>
-                  {project.frontmatter.title}
-                  <p>{project.frontmatter.stack}</p>
-                </h3>
-              </div>
-            </Link>
-          ))}
+          {projectsData.map(project => {
+            console.log(
+              project.frontmatter.thumb.childrenImageSharp[0].gatsbyImageData
+            );
+            const projectImg = getImage(project.frontmatter.thumb?.childrenImageSharp?.GatsbyImage.gatsbyImageData)
+            return (
+              <Link
+                to={"/projects/" + project.frontmatter.slug}
+                key={project.id}
+              >
+                <div>
+                  <GatsbyImage
+                    image={
+                      project.frontmatter.thumb.childrenImageSharp[0]
+                        .gatsbyImageData
+                    }
+                    alt=""
+                  ></GatsbyImage>
+                  <h3>
+                    {project.frontmatter.title}
+                    <p>{project.frontmatter.stack}</p>
+                  </h3>
+                </div>
+              </Link>
+            );
+          })}
         </div>
         <p>Email me at {contact}!</p>
       </div>
@@ -34,13 +50,18 @@ export default function Projects(props) {
 // export page query
 export const query = graphql`
   query MyQuery {
-    projects: allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+    projects: allMarkdownRemark {
       nodes {
         frontmatter {
           title
           stack
           slug
           date
+          thumb {
+            childrenImageSharp {
+              gatsbyImageData(layout: CONSTRAINED)
+            }
+          }
         }
         id
       }
